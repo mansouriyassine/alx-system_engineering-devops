4c71f64d647a7bb4253fe8dcc97cc0b545939436
mansouriyassine <mansouri.yassine1@gmail.com>
2023-10-31 04:55:34 -0700

    4

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,7 +1,3 @@
 #!/usr/bin/env ruby
 # Check if the first argument matches the regular expression pattern
-if ARGV[0] =~ /hbt*n/
-  puts ARGV[0].scan(/hbt*n/).join
-else
-  puts ""
-end
+puts ARGV[0].scan(/hbt+n/).join
--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,7 +1,3 @@
 #!/usr/bin/env ruby
 # Check if the first argument matches the regular expression pattern
-if ARGV[0] =~ /hb*t{1,4}n/
-  puts ARGV[0].scan(/hb*t{1,4}n/).join
-else
-  puts ""
-end
+puts ARGV[0].scan(/hbt*n/).join

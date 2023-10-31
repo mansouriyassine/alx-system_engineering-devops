0943d22054a3f99bd47f39cac42f0a8acf1d4619
mansouriyassine <mansouri.yassine1@gmail.com>
2023-10-31 04:58:54 -0700

    100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
-#  reads a log file containing TextMe app text message transactions and extracts the sender, receiver,
-#  and flags information in the specified format: [SENDER],[RECEIVER],[FLAGS].
+# Reads a log file containing TextMe app text message transactions and extracts the sender, receiver,
+# and flags information in the specified format: [SENDER],[RECEIVER],[FLAGS].
 if ARGV.empty?
   puts "Usage: #{$PROGRAM_NAME} <log_file>"
   exit(1)
@@ -9,11 +9,8 @@ end
 log_file = ARGV[0]
 
 File.open(log_file, 'r').each do |line|
-  sender = line.match(/\[from:([^]]+)\]/)
-  receiver = line.match(/\[to:([^]]+)\]/)
-  flags = line.match(/\[flags:([^]]+)\]/)
-
-  if sender && receiver && flags
-    puts "#{sender[1]},#{receiver[1]},#{flags[1]}"
+  data = line.scan(/\[(?:from:|to:|flags:)(.*?)\]/)
+  if data.length == 3
+    puts data.join(",")
   end
 end

b986aee56a077ebf21cf467f4b8a91593aaef0b8
mansouriyassine <mansouri.yassine1@gmail.com>
2023-12-22 02:32:53 -0800

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,12 @@
-#!/usr/bin/pup
-# Installs Flask version 2.1.0 using pip3. Ensures python3-pip is installed and pip is up-to-date.
+#!/usr/bin/puppet apply
+# Installs Flask version 2.1.0 using pip3. Ensures python3-pip is installed.
+
 package { 'python3-pip':
   ensure => installed,
-}
-
-exec { 'upgrade-pip':
-  command => 'pip3 install --upgrade pip',
-  path    => ['/usr/bin', '/bin'],
 }
 
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => [Package['python3-pip'], Exec['upgrade-pip']],
+  require  => Package['python3-pip'],
 }

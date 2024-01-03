746bead7a52a5ca0549c6e2a76079d5ddf32253d
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-03 02:59:05 -0800

    7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,35 +1,39 @@
-# Nginx - Puppet manifest to install and configure Nginx with a redirect
-class nginx {
-  
+# 7-puppet_install_nginx_web_server.pp
+class nginx_install {
+
+  # Ensure Nginx package is installed
   package { 'nginx':
-    ensure => present,
+    ensure => installed,
   }
 
+  # Ensure Nginx service is running and enabled
   service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    require   => Package['nginx'],
+    ensure => running,
+    enable => true,
+    require => Package['nginx'],
     subscribe => File['/etc/nginx/sites-available/default'],
   }
 
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => template('nginx/default.erb'),
-    notify  => Service['nginx'],
-    require => Package['nginx'],
-  }
-
-  file { '/var/www/html/custom_404.html':
-    ensure  => file,
-    content => 'Ceci n\'est pas une page',
-    require => Package['nginx'],
-  }
-
+  # Serve a custom hello world page
   file { '/var/www/html/index.html':
-    ensure  => file,
-    content => 'Hello World!',
+    ensure  => present,
+    content => "Hello World!\n",
     require => Package['nginx'],
+  }
+
+  # Custom 404 page content
+  file { '/var/www/html/custom_404.html':
+    ensure  => present,
+    content => "Ceci n'est pas une page\n",
+    require => Package['nginx'],
+  }
+
+  # Configure Nginx to serve the custom pages and setup redirection
+  file { '/etc/nginx/sites-available/default':
+    ensure  => present,
+    content => template('nginx/default.conf.erb'),
+    notify  => Service['nginx'],
   }
 }
 
-include nginx
+include nginx_install

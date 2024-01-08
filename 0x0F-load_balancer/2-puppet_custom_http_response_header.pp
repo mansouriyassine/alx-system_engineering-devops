e415f1a543991c087afe9103db0564e617ae6b4e
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-08 07:52:35 -0800

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,33 +1,37 @@
+# task 2 Puppet manifest to configure Nginx with a custom HTTP header
+
 class nginx_custom_header {
   package { 'nginx':
     ensure => installed,
   }
 
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    require   => Package['nginx'],
-    subscribe => File['/etc/nginx/sites-available/default'],
-  }
-
-  file { '/etc/nginx/sites-available/default':
+  file { '/var/www/html/index.nginx-debian.html':
     ensure  => file,
+    content => "Hello World!\n",
     require => Package['nginx'],
-    content => template('nginx/default.erb'),
-    notify  => Service['nginx'],
   }
-  
-  # This exec resource uses an inline template to add the custom header
-  exec { 'insert_custom_header':
-    command => "/bin/echo \"$(/bin/echo -e '\n\tadd_header X-Served-By ${::hostname};')\" | sudo /usr/bin/tee -a /etc/nginx/sites-available/default",
-    unless  => "/bin/grep -q 'X-Served-By' /etc/nginx/sites-available/default",
-    path    => ['/bin', '/usr/bin'],
-    require => File['/etc/nginx/sites-available/default'],
-    notify  => Service['nginx'],
+
+  file { '/usr/share/nginx/html/custom_404.html':
+    ensure  => file,
+    content => "Ceci n'est pas une page\n",
+    require => Package['nginx'],
+  }
+
+  exec { 'nginx_rewrite_rule':
+    command => "sed -i 's/server_name _;/server_name _;\\n\\trewrite ^\\/redirect_me https:\\/\\/github.com\\/ChidiChuks permanent;\\n\\n\\terror_page 404 \\/custom_404.html;\\n\\tlocation = \\/custom_404.html {\\n\\t\\troot \\/usr\\/share\\/nginx\\/html;\\n\\t\\tinternal;\\n\\t}/' /etc/nginx/sites-available/default",
+    require => Package['nginx'],
+  }
+
+  exec { 'nginx_custom_header':
+    command => "sed -i 's/include \\/etc\\/nginx\\/sites-enabled\\/*;/include \\/etc\\/nginx\\/sites-enabled\\/*;\\n\\tadd_header X-Served-By \"$HOSTNAME\";/' /etc/nginx/nginx.conf",
+    require => Package['nginx'],
+  }
+
+  service { 'nginx':
+    ensure  => running,
+    enable  => true,
+    require => Exec['nginx_custom_header'],
   }
 }
 
-node default {
-  include nginx_custom_header
-}
-
+include nginx_custom_header

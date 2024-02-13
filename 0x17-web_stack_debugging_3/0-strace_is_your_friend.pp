480b47472d0cfd9e3949f10ad9bc2e8eafd8975c
mansouriyassine <mansouri.yassine1@gmail.com>
2024-02-13 08:52:57 -0800

    0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # 0.Strace is your friend
 
 exec { 'Fix WordPress config typo':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }

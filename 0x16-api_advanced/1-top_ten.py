5e258226afe05529d85fcbcbe9a01266b913fb57
mansouriyassine <mansouri.yassine1@gmail.com>
2024-02-13 07:03:51 -0800

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+Module to query the Reddit API and print the titles of the first
+10 hot posts listed for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts of a subreddit."""
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'User-Agent': 'Python:subreddit.top.ten:v1.0 (by /u/yourusername)'}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            posts = response.json().get('data', {}).get('children', [])
+            for post in posts:
+                print(post.get('data', {}).get('title'))
+        else:
+            print(None)
+    except requests.RequestException:
+        print(None)
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])

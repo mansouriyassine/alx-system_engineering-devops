6162ca63424ebe33224fb0a7f16d6ac86398abe3
mansouriyassine <mansouri.yassine1@gmail.com>
2024-02-13 08:39:31 -0800

    0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,6 @@
+# 0. Strace is your friend
 
+exec { 'fix config typo':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
